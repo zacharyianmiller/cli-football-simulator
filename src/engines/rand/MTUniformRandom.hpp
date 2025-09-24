390581cb9cbfd8fb425f417bc47f5b51f519b47f
Zachary Miller <zacharyianmiller1@gmail.com>
2025-09-24 08:54:56 -0500

    mtrand refactor

--- src/engines/rand/MTUniformRandom.hpp
+++ src/engines/rand/MTUniformRandom.hpp
@@ -9,16 +9,16 @@ class MTUniformRandom
 public:
 
     MTUniformRandom(const FloatType begin, const FloatType end) :
-    generator(std::make_unique<std::mt19937>(std::time(nullptr))),
-    dist(std::make_unique<std::uniform_real_distribution<FloatType>>(begin, end)) 
+    dist(std::make_unique<std::uniform_real_distribution<FloatType>>(begin, end)),
+    generator(std::time(nullptr))
     {
     }
 
-    FloatType operator ()() { return (*dist)(*generator); }
+    FloatType operator ()() { return (*dist)(generator); }
 
 private:
 
-    std::unique_ptr<std::mt19937> generator;
+    std::mt19937 generator;
     std::unique_ptr<std::uniform_real_distribution<FloatType>> dist;
-
+    
 };
